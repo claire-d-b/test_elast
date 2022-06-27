4f482baf194c810cddc54b124c99033c552dc540
clairedb <claire.deberny@essca.eu>
2022-06-27 18:53:36 +0200

    wip

---
+++ requete.py
@@ -0,0 +1,22 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+client = client = Elasticsearch(
+    "https://localhost:9200"
+)
+
+s = Search(using=client, index="question") \
+    .filter("term", category="search") \
+    .query("match", title="Je n'arrete pas de tousser")   \
+    .exclude("match", description="beta")
+
+s.aggs.bucket('per_tag', 'terms', field='tags') \
+    .metric('max_lines', 'max', field='lines')
+
+response = s.execute()
+
+for hit in response:
+    print(hit.meta.score, hit.title)
+
+for tag in response.aggregations.per_tag.buckets:
+    print(tag.key, tag.max_lines.value)
