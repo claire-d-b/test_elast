10517860605fb0ad8707636cb2692717940005d6
clairedb <claire.deberny@essca.eu>
2022-06-27 19:07:12 +0200

    wip

--- requete.py
+++ requete.py
@@ -6,9 +6,7 @@ client = client = Elasticsearch(
 )
 
 s = Search(using=client, index="question") \
-    .filter("term", category="search") \
     .query("match", title="J'ai mal à l'os du bas")   \
-    .exclude("match", description="beta")
 
 s.aggs.bucket('per_tag', 'terms', field='tags') \
     .metric('max_lines', 'max', field='lines')
@@ -19,4 +17,18 @@ for hit in response:
     print(hit.meta.score, hit.title)
 
 for tag in response.aggregations.per_tag.buckets:
+    print(tag.key, tag.max_lines.value)
+
+s2 = Search(using=client, index="question") \
+    .query("match", tags="J'ai mal à l'os du bas")   \
+
+s2.aggs.bucket('per_tag', 'terms', field='tags') \
+    .metric('max_lines', 'max', field='lines')
+
+response2 = s2.execute()
+
+for hit in response2:
+    print(hit.meta.score, hit.title)
+
+for tag in response2.aggregations.per_tag.buckets:
     print(tag.key, tag.max_lines.value)
